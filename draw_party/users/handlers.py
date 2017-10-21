02c296d11422d5571422c602b8f3154abcc745ca
SimKev2 <simkev2@gmail.com>
2017-10-21 12:47:50 -0500

    falcon-base  - Fixed MIME_TYPES selector since we only have a few types  - Added tests for the static handler

--- draw_party/users/handlers.py
+++ draw_party/users/handlers.py
@@ -1,16 +1,16 @@
-import logging
-import mimetypes
 import os
 
 import falcon
 
 
-log = logging.getLogger(__name__)
-
-
 class Homepage(object):
     """Handler for the create/join homepage."""
 
+    MIME_TYPES = {
+        'css': 'text/css',
+        'html': 'text/html',
+        'js': 'application/javascript'
+    }
     STATIC_DIR = os.path.join(
         os.path.dirname(os.path.dirname(__file__)), 'client')
 
@@ -23,14 +23,14 @@ class Homepage(object):
         :type req: falcon.Request
         :param resp: The response object to be returned.
         :type resp: falcon.Response
+        :param filename: The path to the file to open.
+        :type filename: str
         :rtype: None
         """
-        log.error(req.path)
-        fn = os.path.join(self.STATIC_DIR, filename)
-        resp.set_header(
-            'Content-Type',
-            mimetypes.guess_type(fn, strict=False)[0] or 'text/html')
-
+        fn = os.path.join(self.STATIC_DIR, filename or 'index.html')
         resp.status = falcon.HTTP_200
+        resp.set_header(
+            'Content-Type', self.MIME_TYPES.get(fn.rsplit('.', 1)[-1]))
+
         with open(fn, 'rb') as f:
             resp.body = f.read()
